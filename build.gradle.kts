ada3469cf3a98279b78c639c126675bb09c44965
Outfit8TSB <sunnybunnies09@naver.com>
2024-09-13 20:05:25 +0900

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id 'java'
+}
+
+group 'com.yourname.viaproxyplugin'
+version '1.0-SNAPSHOT'
+
+repositories {
+    mavenCentral()
+    maven { url "https://repo.viaversion.com" }
+}
+
+dependencies {
+    implementation "net.raphimc:ViaProxy:x.x.x" // Replace x.x.x with the latest version
+}
+
+sourceCompatibility = '1.8'
+targetCompatibility = '1.8'
+
+jar {
+    manifest {
+        attributes(
+                'Main-Class': 'com.yourname.viaproxyplugin.MainClass' // Replace with your main class
+        )
+    }
+}
